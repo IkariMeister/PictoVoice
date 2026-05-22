acc16ab1cea611c1f91d463d846761ce0c5a7f5f
Juan Carlos Garcia Seco <jcgseco@gmail.com>
2026-05-23 00:04:48 +0200

    chore: add detekt lint tooling and CI check

--- pictovoice/build.gradle.kts
+++ pictovoice/build.gradle.kts
@@ -4,4 +4,39 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    val detektBaselineFile = file("$rootDir/config/detekt/${project.path.replace(':', '_')}-baseline.xml")
+
+    extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        allRules = false
+        parallel = true
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        baseline = detektBaselineFile
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        setSource(
+            files(
+                fileTree(projectDir) {
+                    include("src/**/*.kt", "src/**/*.kts")
+                }
+            )
+        )
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+        setSource(
+            files(
+                fileTree(projectDir) {
+                    include("src/**/*.kt", "src/**/*.kts")
+                }
+            )
+        )
+    }
 }
